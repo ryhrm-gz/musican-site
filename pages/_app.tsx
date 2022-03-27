57989a6a5a71e13b731b40620ad27401716c8a96
ryhrm-gz <gyozaoisii@icloud.com>
2022-03-27 16:36:23 +0900

    add seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,30 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import { DefaultSeo } from "next-seo";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
   return (
     <>
+      <DefaultSeo
+        defaultTitle="musican"
+        canonical="https://musican.vercel.app/"
+        description="ミュージシャンのための楽曲管理アプリ"
+        twitter={{
+          cardType: "summary",
+        }}
+        openGraph={{
+          type: "website",
+          url: "https://musican.vercel.app/",
+          site_name: "musican",
+          images: [
+            { url: "./icon_512x512.png", width: 512, height: 512, alt: "Icon" },
+          ],
+        }}
+      />
       <Head>
-        <title>musican</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
